6b5c4117ca5d017fda58684c291bee230e3d793e
Christopher Phoenix <hello@chrisphoenix.dev>
2026-07-30 19:29:37 -0700

    feat: add basic math operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
 // script.js
 
 console.log('hello, world!');
+
+
+// math operator functions
+
+function add(addendOne, addendTwo) {
+  return addendOne + addendTwo;
+}
+
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
+
+function multiply(factorOne, factorTwo) {
+  return factorOne * factorTwo;
+}
+
+function divide(dividend, divisor) {
+  if (divisor === 0) {
+    throw new Error('Division by zero is not allowed.');
+  }
+
+  return dividend / divisor;
+}
