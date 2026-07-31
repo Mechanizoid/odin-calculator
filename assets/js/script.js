59a72ae460f580878cea7222b6f147cd3d999580
Christopher Phoenix <hello@chrisphoenix.dev>
2026-07-31 10:06:55 -0700

    fix: throw error when provided an invalid operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,14 @@ function operate(firstNumber, secondNumber, operator) {
   switch(operator) {
   case '+':
     return add(firstNumber, secondNumber);
-  case'-':
+  case '-':
     return subtract(firstNumber, secondNumber);
   case '*':
     return multiply(firstNumber, secondNumber);
   case '/':
     return divide(firstNumber, secondNumber);
+  default:
+    throw new Error('Invalid operator');
   }
 }
 
