8b293f94f1cc2988c54525ce71a237eca033bb41
Christopher Phoenix <hello@chrisphoenix.dev>
2026-08-01 10:56:16 -0700

    style: fix case statement indentation offset
    Emacs defaults to indenting case statements to the same offset as
    the opening switch statement. This behavior conflicted with the
    style expected by typescript-language-server, so I updated the
    project configuration to apply an indentation offset of two spaces
    for case statements relative to the opening switch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,16 +24,16 @@ function divide(dividend, divisor) {
 
 function operate(firstNumber, secondNumber, operator) {
   switch(operator) {
-  case '+':
-    return add(firstNumber, secondNumber);
-  case '-':
-    return subtract(firstNumber, secondNumber);
-  case '*':
-    return multiply(firstNumber, secondNumber);
-  case '/':
-    return divide(firstNumber, secondNumber);
-  default:
-    throw new Error(`Invalid operator: ${operator}`);
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+    default:
+      throw new Error(`Invalid operator: ${operator}`);
   }
 }
 
@@ -54,44 +54,44 @@ function clearAll() {
 
 function processKey(key) {
   switch(key) {
-  case '0': case '1': case '2': case '3': case '4': case '5':
-  case '6': case '7': case '8': case '9': case '.':
-    if (resultDisplayed) {
-      resultDisplayed = false;
-      display.textContent = '';
-    }
-    display.textContent = display.textContent + key;
-    break;
-  case '(-)':
-    toggleNegative();
-    break;
-  case '*': case '/': case '+': case '-':
-    if (!secondOperand) {
-      firstOperand = Number(display.textContent);
-      operator = key;
-      resultDisplayed = true;
+    case '0': case '1': case '2': case '3': case '4': case '5':
+    case '6': case '7': case '8': case '9': case '.':
+      if (resultDisplayed) {
+        resultDisplayed = false;
+        display.textContent = '';
+      }
+      display.textContent = display.textContent + key;
       break;
-    }
-  case '=':
-    if (!secondOperand) {
-      secondOperand = Number(display.textContent);
-    }
-    
-    result = operate(firstOperand, secondOperand, operator);
-    display.textContent = result;
-    resultDisplayed = true;
+    case '(-)':
+      toggleNegative();
+      break;
+    case '*': case '/': case '+': case '-':
+      if (!secondOperand) {
+        firstOperand = Number(display.textContent);
+        operator = key;
+        resultDisplayed = true;
+        break;
+      }
+    case '=':
+      if (!secondOperand) {
+        secondOperand = Number(display.textContent);
+      }
 
-    if (key != '=') {
-      operator = key;
-      firstOperand = result;
-    }
-    break;
-  case 'Del':
-    display.textContent = display.textContent.slice(0, -1);
-    break;
-  case 'AC':
-    clearAll();
-    break;
+      result = operate(firstOperand, secondOperand, operator);
+      display.textContent = result;
+      resultDisplayed = true;
+
+      if (key != '=') {
+        operator = key;
+        firstOperand = result;
+      }
+      break;
+    case 'Del':
+      display.textContent = display.textContent.slice(0, -1);
+      break;
+    case 'AC':
+      clearAll();
+      break;
   }
 }
 
